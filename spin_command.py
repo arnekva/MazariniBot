29f177ec9ef6e3caa694d34ed3354e0aa9ee24c7
Arne Kvaleberg <arnekva@gmail.com>
2021-03-09 10:24:54 +0000

    Highscores now sent as one message

--- spin_command.py
+++ spin_command.py
@@ -9,15 +9,10 @@ from urllib.error import HTTPError
 async def spin(message):
   elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
   weights = [0.4 , 0.3 , 0.145 , 0.08, 0.04, 0.02, 0.008, 0.004, 0.0015, 0.001, 0.0005]
-  randMin = random.randint(0,5)
-  # randMin = numpy.random.gamma()
+
   randSek = random.randint(0,59)
   supernum = choice(elements, p=weights)
 
-  # if(message.author.id == 239154365443604480):
-  #   randSek = random.randint(0,59)
-  #   st = "Eivind spant fidget spinneren i " + str(randSek) + " sekund før " + random.choice(eivindSpinner)
-  # else:
   rnd = random.randint(0,100)
   if(rnd < 15):
       st = message.author.name + " spant fidget spinneren sin i " + str(randSek) + " sekund før " + random.choice(eivindSpinner)
@@ -26,12 +21,14 @@ async def spin(message):
       await message.channel.send(st)
       tr = str(supernum)+str(randSek)
       await addSpinToDb(message, tr)
-  if(randMin == 10 and randSek == 59 and rnd > 10):
+  if(supernum == 10 and randSek == 59 and rnd > 15):
     await message.channel.send("gz")
-  elif(randMin == 0 and randSek == 0):
+  elif(supernum == 0 and randSek == 0):
     await message.channel.send("Du suge faktisk.")  
 
 async def addSpinToDb(message, timeString):
+  if(len(str(timeString)) == 2):
+    timeString = timeString[:1] + "0" + timeString[1:]
   srch = "spin" + str(message.author.name)
   try:
     currentVal = db[srch]
@@ -45,7 +42,8 @@ async def addSpinToDb(message, timeString):
 
 async def readHighscores(message):
   keys = db.prefix("spin")
-  await message.channel.send("*** HIGHSCORES ***")
+  scores = ""
+  scores += "*** HIGHSCORES ***"
 
   for key in keys:
     val = db[key]
@@ -55,7 +53,9 @@ async def readHighscores(message):
     elif(len(timeToPrint) == 3):
       timeToPrint = "0" + timeToPrint[0:1] + ":" + timeToPrint[1:]
     name = key.replace("spin", "")
-    await message.channel.send(name + ": " + timeToPrint)
+    printStr = name + ": " + timeToPrint
+    scores += "\n"+printStr
+  await message.channel.send(scores)
   
 
 
